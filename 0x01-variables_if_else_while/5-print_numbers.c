b906621dacad9e29b41aaba957695940cc87d8be
Sameh Hassan <sameh7assan94@gmail.com>
2023-03-17 19:47:39 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - function
+*
+* Return: 0 always
+*/
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	printf("%i", digit);
+	putchar('\n');
+
+	return (0);
+}
